99ed57e1a8f60a1a1d31e7f9a783afbe8766473f
Andreas <andreas.axelsson@superoffice.se>
2023-10-03 14:43:13 +0200

    Initial

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@ using System.Text.Json;
 Console.WriteLine("Here is the correct json even in .NET 8, the Test controller will return incorrect json in browser window.");
 //This will output the correct json in both .NET 6/7 and .NET 8
 //The Test controller will return the incorrect json in .NET 8
-Console.WriteLine(JsonSerializer.Serialize(new Test1()));
+var options = new JsonSerializerOptions();
+options.Converters.Add(new ObjectJsonConverter());
+Console.WriteLine(JsonSerializer.Serialize(new Test1(), options));
 
 Console.WriteLine();
 
